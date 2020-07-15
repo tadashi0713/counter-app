efa141d5107149782fbd4fa446da1d0f0855c7de
Tadashi Nemoto <tadashi.nemoto@exwzd.com>
2020-07-15 18:21:10 +0900

    Add 1st tests

--- tests/step1/counter.ts
+++ tests/step1/counter.ts
@@ -1,8 +1,13 @@
 import "testcafe"
+import { Selector } from "testcafe";
 
 fixture `CounterFixture`
-    .page `http://localhost:3000`;
+  .page `http://localhost:3000`;
 
-test('', async t => {
-
+test('Add 1 item', async t => {
+  const firstPlusButton = Selector('.fa-plus-circle').nth(1)
+  const cartItemBadge = Selector('.badge-info')
+  await t
+    .click(firstPlusButton)
+    .expect(cartItemBadge.innerText).eql("1")
 });
